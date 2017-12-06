09536e0f366a9d54f9147e61e24bb6f1fe1e4f71
islamTaha12 <tahai@codescalers.com>
2017-12-06 15:13:22 +0200

    update

--- test/deploy_zstor_in_travis/run_tests.sh
+++ test/deploy_zstor_in_travis/run_tests.sh
@@ -3,7 +3,8 @@ if [ "$TRAVIS_EVENT_TYPE" == "cron" ] || [ "$TRAVIS_EVENT_TYPE" == "api" ]; then
 
     if [ "$action" == "before" ]; then
         echo "[+] Deploy zstordb servers"
-        bash test/deploy_zstor_in_travis/manual_deployment.sh  ${number_of_servers} ${iyo_client_id} ${iyo_secret} ${iyo_organization} ${iyo_namespace}
+        sudo bash test/deploy_zstor_in_travis/manual_deployment.sh  ${number_of_servers} ${iyo_client_id} ${iyo_secret} ${iyo_organization} ${iyo_namespace}
+        cat /gopath/src/github.com/zero-os/0-stor/cmd/zstor/config.yaml
     elif [ "$action" == "test" ]; then
         echo " [*] Execute test case"
         cd test && export PYTHONPATH='./' && nosetests-3.4 -vs --logging-level=WARNING --progressive-with-bar --rednose $TEST_CASE --tc-file test_suite/config.ini --tc=main.number_of_servers:${number_of_servers} --tc=main.number_of_files:${number_of_files} --tc=main.default_config_path:${default_config_path} --tc=main.iyo_user2_id:${iyo_user2_id} --tc=main.iyo_user2_secret:${iyo_user2_secret} --tc=main.iyo_user2_username:${iyo_user2_username}
